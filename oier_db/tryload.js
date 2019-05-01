51f78de1100ceed2ea59223c7ee202e74811ecd1
fjzzq2002 <fjzzq2002@163.com>
2019-05-01 23:20:15 +0800

    compressed localstorage
    localstorage may exceed quota, add a compresser

--- oier_db/tryload.js
+++ oier_db/tryload.js
@@ -1,7 +1,9 @@
+//warning: do not use chrome developer tools to view local storage, chrome may crash
+
 var lcc=localStorage.getItem("oierdb_data");
 if(lcc)
 {
-    lcc=JSON.parse(lcc);
+    lcc=JSON.parse(LZString.decompressFromUTF16(lcc));
     if(lcc['ver']!=db_ver)
         console.log('local data is out of date!'),lcc=null;
 }
@@ -11,7 +13,7 @@ if(!lcc)
     $.ajaxSetup({async:false});
     $.getScript("data.js");
     lcc={};lcc['ver']=db_ver;lcc['tbl']=tb;
-    localStorage.setItem('oierdb_data',JSON.stringify(lcc));
+    localStorage.setItem('oierdb_data',LZString.compressToUTF16(JSON.stringify(lcc)));
     console.log('stored in localstorage.');
 }
 else
